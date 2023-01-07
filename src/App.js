fe017ffc8883acc70817fa599238c570d3a309a7
Elizabethucar <elizg77@hotmail.com>
2023-01-07 21:48:47 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Support from "./components/Support";
+import AllinOne from "./components/AllinOne";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Support />
+      <AllinOne />
+      <Pricing />
+      <Footer />
     </>
   );
 }
